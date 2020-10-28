55ec321059d13fc21df5be076c1e3850d866b2d7
FiCacador <ficacador@gmail.com>
2020-10-28 17:23:52 +0000

    Led as WiFi status, ntp time

--- examples/Discover32_Sensors_WiFi/Discover32_Sensors_WiFi.ino
+++ examples/Discover32_Sensors_WiFi/Discover32_Sensors_WiFi.ino
@@ -6,6 +6,7 @@
 #include <Adafruit_Si7021.h>
 #include <WiFi.h>
 #include <WebServer.h>
+#include <time.h>
   
 #define I2C_SDA 21
 #define I2C_SCL 22
@@ -18,8 +19,13 @@ PCD85063TP RTclock;
 Adafruit_Si7021 si7021;
 WebServer server(80);
 
+
 const char* ssid     = "yourssid";
 const char* password = "yourpassword";
+bool led_enabled = false;
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 0;
+const int daylightOffset_sec = 3600;
 
 void setup() {
   Wire.begin(I2C_SDA, I2C_SCL);
@@ -42,12 +48,9 @@ void setup() {
 
   RTclock.begin();
 
-  RTclock.stopClock();
-  RTclock.fillByYMD(2020,10,9);
-  RTclock.fillByHMS(18,00,00);
-  RTclock.fillDayOfWeek(FRI);
-  RTclock.setTime();
-  RTclock.startClock();
+  
+
+  pinMode(17, OUTPUT);
 
 }
 
@@ -57,11 +60,16 @@ void loop() {
     Serial.println(ssid);
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
+      led_enabled = !led_enabled;
+      digitalWrite(17, led_enabled);
       delay(500);
     }
     Serial.println("WiFi connected.");
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
+
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    updateRTclock();
 
     server.on("/", handleRoot);
     server.on("/api", handleAPI);
@@ -69,8 +77,44 @@ void loop() {
       server.send(404, "text/plain", "Page not found");
     });
     server.begin();
+
+    digitalWrite(17, LOW);
   }
   server.handleClient();
+}
+
+void updateRTclock() {
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  
+  RTclock.stopClock();
+  RTclock.fillDayOfWeek(timeinfo.tm_wday);
+  RTclock.fillByYMD(timeinfo.tm_year + 1900, timeinfo.tm_mon + 1, timeinfo.tm_mday);
+  RTclock.fillByHMS(timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+  RTclock.setTime();
+  RTclock.startClock();
+}
+
+char* convertDayOfWeek(int nDayOfWeek) {
+  switch (nDayOfWeek) {
+    case 0:
+      return "Sunday";
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+  }
 }
 
 void handleRoot() {
@@ -83,23 +127,23 @@ void handleRoot() {
   char message[1000];
 
   snprintf(message, 1000, 
-    "<html>\
-    <head>\
-      <meta http-equiv='refresh' content='5'/>\
-      <title>Discover Board</title>\
-    </head>\
-    <body>\
-      <h1>Discover Board Sensors</h1>\
-      <p>%d/%d/%d %02d:%02d:%02d</p>\
-      <p>Temperature: %.2f C</p>\
-      <p>Humidity: %.1f %%</p>\
-      <p>Pressure: %.1f Pa</p>\
-      <p>Altitude: %.1f m</p>\
-      <p>X acceleration: %.2f m/s^2</p>\
-      <p>Y acceleration: %.2f m/s^2</p>\
-    </body>\
-  </html>",
-  RTclock.year + 2000, RTclock.month, RTclock.dayOfMonth, RTclock.hour, RTclock.minute, RTclock.second,
+"<html>\
+  <head>\
+    <meta http-equiv='refresh' content='5'/>\
+    <title>Discover Board</title>\
+  </head>\
+  <body>\
+    <h1>Discover Board Sensors</h1>\
+    <p>%s %d/%d/%d %02d:%02d:%02d</p>\
+    <p>Temperature: %.2f C</p>\
+    <p>Humidity: %.1f %%</p>\
+    <p>Pressure: %.1f Pa</p>\
+    <p>Altitude: %.1f m</p>\
+    <p>X acceleration: %.2f m/s^2</p>\
+    <p>Y acceleration: %.2f m/s^2</p>\
+  </body>\
+</html>",
+  convertDayOfWeek(RTclock.dayOfWeek), RTclock.year + 2000, RTclock.month, RTclock.dayOfMonth, RTclock.hour, RTclock.minute, RTclock.second,
     si7021.readTemperature(), si7021.readHumidity(), bmp280.readPressure(), bmp280.readAltitude(1013.25),
     accel.acceleration.x, accel.acceleration.y);
 
@@ -117,6 +161,7 @@ void handleAPI() {
 
   snprintf(message, 1000, 
 "{\
+\"dayOfWeek\": \"%s\",\
 \"year\": %d,\
 \"month\": %d,\
 \"day\": %d,\
@@ -129,8 +174,8 @@ void handleAPI() {
 \"altitude\": %.1f,\
 \"x-accel\": %.2f,\
 \"y-accel\": %.2f\
-}"
-, RTclock.year + 2000, RTclock.month, RTclock.dayOfMonth, RTclock.hour, RTclock.minute, RTclock.second,
+}", 
+  convertDayOfWeek(RTclock.dayOfWeek), RTclock.year + 2000, RTclock.month, RTclock.dayOfMonth, RTclock.hour, RTclock.minute, RTclock.second,
     si7021.readTemperature(), si7021.readHumidity(), bmp280.readPressure(), bmp280.readAltitude(1013.25),
     accel.acceleration.x, accel.acceleration.y);
 

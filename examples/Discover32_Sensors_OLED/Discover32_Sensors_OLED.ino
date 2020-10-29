dd5540cbf4c7ba7146feb4b3359dfa61c3125386
FiCacador <ficacador@gmail.com>
2020-10-29 17:37:30 +0000

    Add comments

--- examples/Discover32_Sensors_OLED/Discover32_Sensors_OLED.ino
+++ examples/Discover32_Sensors_OLED/Discover32_Sensors_OLED.ino
@@ -8,13 +8,13 @@
 #include <WiFi.h>
 #include <time.h>
 
-#define I2C_SDA 21
-#define I2C_SCL 22
-#define BMP_I2C_ADDR 0x76
-#define LSM_I2C_ADDR 0x6B
-#define SSD_I2C_ADDR 0x3C
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
+#define I2C_SDA 21                      // I2C data line
+#define I2C_SCL 22                      // I2C clock line
+#define BMP_I2C_ADDR 0x76               // BMP280 I2C address
+#define LSM_I2C_ADDR 0x6B               // LSM6DS3 I2C address
+#define SSD_I2C_ADDR 0x3C               // SSD1306 I2C address
+#define SCREEN_WIDTH 128                // OLED screen width
+#define SCREEN_HEIGHT 64                // OLED screen height
 
 Adafruit_BMP280 bmp280;
 Adafruit_LSM6DS3 lsm6ds3;
@@ -22,107 +22,116 @@ PCD85063TP RTclock;
 Adafruit_Si7021 si7021;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-const char* ssid     = "yourssid";
-const char* password = "yourpassword";
+const char* ssid     = "yourssid";        // replace with the name of the WiFi netwoork
+const char* password = "yourpassword";    // replace with the WiFi network password
+const char* ntpServer = "pool.ntp.org";   // ntp pool server
+const long gmtOffset = 0;                 // Greenwich Mean Time (GMT) offset, in hours
+float seaLevelhPa = 1025.0;               // local sea level pressure, in hPa, to adjust altitude
 bool led_enabled = false;
-const char* ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = 0;           // Greenwich Mean Time (GMT) offset
-const int daylightOffset_sec = 0;       // 3600 during summer time (DST)
 
+/* Run setup once */
 void setup() {
   Wire.begin(I2C_SDA, I2C_SCL);
   Serial.begin(115200);
 
-  Serial.println("Discover Board Sensors OLED test");
+  Serial.println("Discover32 Sensors OLED test");
 
-  if (!bmp280.begin(BMP_I2C_ADDR, BMP280_CHIPID)) {
+  if (!bmp280.begin(BMP_I2C_ADDR, BMP280_CHIPID)) {                 // try to initialize BMP280 sensor
     Serial.println("Could not find a valid BMP280 sensor!");
-    while (1);
+    while (1);                                                      // loop forever
   }
-  if (!lsm6ds3.begin_I2C(LSM_I2C_ADDR, &Wire, LSM6DS3_CHIP_ID)) {
+  if (!lsm6ds3.begin_I2C(LSM_I2C_ADDR, &Wire, LSM6DS3_CHIP_ID)) {   // try to initialize LSM6DS3 sensor
     Serial.println("Could not find a valid LSM6DS3 sensor!");
-    while (1);
+    while (1);                                                      // loop forever
   }
-  if (!si7021.begin()) {
+  if (!si7021.begin()) {                                            // try to initialize Si7021 sensor
     Serial.println("Could not find a valid Si7021 sensor!");
-    while (1);
+    while (1);                                                      // loop forever
   }
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SSD_I2C_ADDR)) {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SSD_I2C_ADDR)) {          // try to initialize SSD1306 display
     Serial.println("Could not find a valid SSD1306 screen!");
-    while (1);
+    while (1);                                                      // loop forever
   }
-  RTclock.begin();
+  RTclock.begin();                                                  // Initialize Real-Time clock
 
-  pinMode(17, OUTPUT);
-  digitalWrite(17, HIGH);       // turn off led (logic inverted)
+  pinMode(17, OUTPUT);                                              // set led pin 17 as output
+  digitalWrite(17, !LOW);                                           // turn off led (logic inverted)
 
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,4);
-  display.println("Connecting to");
+  display.clearDisplay();                                           // clear the display buffer
+  display.setTextSize(1);                                           // set text size to small (1:1 scale)
+  display.setTextColor(SSD1306_WHITE);                              // set draw color to white
+  display.setCursor(0,4);                                           // start at position (X,Y), starting from top left
+  display.println("Connecting to");                                 // print line to display buffer
   display.println(ssid);
-  display.display();
+  display.display();                                                // update display to show what is on the buffer
 
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, password);                                       // connect to the specified WiFi network
   int count = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    led_enabled = !led_enabled;
-    digitalWrite(17, led_enabled);
-    if (count == 20) {
-      WiFi.mode(WIFI_OFF);
-      digitalWrite(17, HIGH);       // turn off led (logic inverted)
+  while (WiFi.status() != WL_CONNECTED) {                           // verify it hasn't connected yet
+    led_enabled = !led_enabled;                                     // toggle led state
+    digitalWrite(17, led_enabled);                                  // turn led on or off
+    if (count == 20) {                                              // verify if the loop has been running for 10 seconds (20x500ms)
+      WiFi.mode(WIFI_OFF);                                          // turn off WiFi
+      digitalWrite(17, !LOW);                                       // turn off led (logic inverted)
       
       display.println("Connection Failed");
-      display.display();
+      display.display(); 
       
-      setSpecificTime();
+      setSpecificTime();                                            // set a specified time and date
 
-      delay(2000);
+      delay(2000);                                                  // wait 2 seconds
       return;
     }
-    count++;
-    delay(500);
+    count++;                                                        // increase counter
+    delay(500);                                                     // wait 500 ms
   }
 
-  digitalWrite(17, LOW);       // turn on led (logic inverted)
-  
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  digitalWrite(17, !HIGH);                                          // turn on led (logic inverted)
 
   display.println("Setting ntp time");
   display.display();
+    
+  configTime(gmtOffset * 3600, 0, ntpServer);                       // configure time
 
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    display.clearDisplay();
+  struct tm timeinfo;                                               // create time structure
+  
+  if(!getLocalTime(&timeinfo)){                                     // try to get time info and save it on the timeinfo structure
     display.println("Failed to obtain time");
     display.display();
 
+    WiFi.disconnect(true);                                          // disconnect from the WiFi network
     WiFi.mode(WIFI_OFF);
-    digitalWrite(17, HIGH);       // turn off led (logic inverted)
+    digitalWrite(17, !LOW);                                         // turn off led (logic inverted)
     
-    setSpecificTime;
+    setSpecificTime;                                                // set a specified time and date
 
     delay(2000);
     return;
   }
 
-  RTclock.stopClock();
-  RTclock.fillDayOfWeek(timeinfo.tm_wday);
-  RTclock.fillByYMD(timeinfo.tm_year + 1900, timeinfo.tm_mon + 1, timeinfo.tm_mday);
-  RTclock.fillByHMS(timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
-  RTclock.setTime();
-  RTclock.startClock();
+  if (timeinfo.tm_isdst > 0) {
+    configTime(gmtOffset * 3600, 3600, ntpServer);
+    getLocalTime(&timeinfo);
+  }
 
+  RTclock.stopClock();                                                                // stop the real-time clock
+  RTclock.fillDayOfWeek(timeinfo.tm_wday);                                            // fill week day from timeinfo structure
+  RTclock.fillByYMD(timeinfo.tm_year + 1900, timeinfo.tm_mon + 1, timeinfo.tm_mday);  // fill year, month and day from timeinfo structure
+  RTclock.fillByHMS(timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);              // fill hour, minute and second from timeinfo structure
+  RTclock.setTime();                                                                  // set the filled time
+  RTclock.startClock();                                                               // start the real-time clock
+
+  WiFi.disconnect(true);
   WiFi.mode(WIFI_OFF);
 
   delay(1000);
-  digitalWrite(17, HIGH);       // turn off led (logic inverted)
+  digitalWrite(17, !LOW);
 }
 
+/* Loop what is shown on screen */
 void loop() {
   drawlogo();
-  showday();
+  showdate();
   showtime();
   showtemperature();
   showhumidity();
@@ -132,6 +141,7 @@ void loop() {
   showyaccel();
 }
 
+/* Set a specific time and date, in case ntp time can't be reached */
 void setSpecificTime(void) {
   RTclock.stopClock();
   RTclock.fillByYMD(2020,10,9);
@@ -141,6 +151,7 @@ void setSpecificTime(void) {
   RTclock.startClock();
 }
 
+/* Logo bitmap */
 #define LOGO_HEIGHT   58
 #define LOGO_WIDTH    60
 static const uint8_t logo_bmp[] = {
@@ -175,31 +186,32 @@ static const uint8_t logo_bmp[] = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
-
+/* Show the logo and scroll it on screen */
 void drawlogo(void) {
-  display.clearDisplay();
+  display.clearDisplay();                       // clear the display buffer
   
-  display.drawBitmap(
-    10,
-    (display.height() - LOGO_HEIGHT) / 2,
+  display.drawBitmap(                           // send a bitmap to the display buffer
+    10,                                         // X position
+    (display.height() - LOGO_HEIGHT) / 2,       // Y position
     logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
     
-  display.display();
+  display.display();                            // update display to show what is on the buffer
 
-  display.startscrollright(0x00, 0x0F);
-  delay(3000);
-  display.stopscroll();
+  display.startscrollright(0x00, 0x0F);         // start scrolling objects on the display to the right
+  delay(3000);                                  // wait 3 seconds
+  display.stopscroll();                         // stop scrolling
 }
 
-void showday(void) {
-  RTclock.getTime();
+/* Show current date on screen */
+void showdate(void) {
+  RTclock.getTime();                            // get the current time from the real-time clock
   
-  display.clearDisplay();
-  display.setTextSize(2);
+  display.clearDisplay();                       // clear the display buffer
+  display.setTextSize(2);                       // set text size to a medium size (2:1 scale)
   display.setTextColor(SSD1306_WHITE);
   
   display.setCursor(0,4);
-  switch (RTclock.dayOfWeek) {
+  switch (RTclock.dayOfWeek) {                  // switch based on week day from the real-time clock
     case MON:
       display.print("Monday");
       break;
@@ -224,16 +236,17 @@ void showday(void) {
   }
 
   display.setCursor(0,44);
-  display.print(RTclock.year+2000);
+  display.print(RTclock.year + 2000);         // print year from RTclock + 2000 to display buffer
   display.print("/");
-  display.print(RTclock.month);
+  display.print(RTclock.month);               // print month from RTclock to display buffer
   display.print("/");
-  display.print(RTclock.dayOfMonth);
-  display.display();
+  display.print(RTclock.dayOfMonth);          // print day from RTclock to display buffer
+  display.display();                          // update display to show what is on the buffer
   
   delay(3000);
 }
 
+/* Show current time on screen */
 void showtime(void) {
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
@@ -243,7 +256,7 @@ void showtime(void) {
   int RTsecond;
   
   int count = 0;
-  while (count < 60) {
+  while (count < 60) {                        // loop for 3 seconds (60x50ms)
     count++;
     
     RTclock.getTime();
@@ -252,150 +265,156 @@ void showtime(void) {
     RTminute = RTclock.minute;
     RTsecond = RTclock.second;
   
-    display.clearDisplay();
+    display.clearDisplay();                   // clear the display buffer
     
     display.setCursor(14,24);
 
     display.print(RThour);
     display.print(":");
     
-    if (RTminute < 10) {
-      display.print("0");
+    if (RTminute < 10) {                      // if the minutes have less than 2 digits
+      display.print("0");                     // print to display buffer a 0 before
     }
     display.print(RTminute);
     display.print(":");
     
-    if (RTsecond < 10) {
-      display.print("0");
+    if (RTsecond < 10) {                      // if the seconds have less than 2 digits
+      display.print("0");                     // print to display buffer a 0 before
     }
     display.print(RTsecond);
 
-    display.display();
+    display.display();                        // update display to show what is on the buffer
     
-    delay(50);
+    delay(50);                                // wait 50 ms
   }
 }
 
+/* Show current temperature on screen */
 void showtemperature(void) {
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
   
   int count = 0;
-  while (count < 15) {
+  while (count < 15) {                          // loop for 3 seconds (15x200ms)
     count++;
     
-    display.clearDisplay();
+    display.clearDisplay();                     // clear the display buffer
     
     display.setCursor(0,4);
     display.print("Temp:");
   
     display.setCursor(40,44);
-    display.print(si7021.readTemperature());
+    display.print(si7021.readTemperature());    // print to display buffer the temperature value from Si7021 sensor
     display.print(" C");
     display.display();
     
-    delay(200);
+    delay(200);                                 // wait 200 ms
   }
 }
 
+/* Show current humidity on screen */
 void showhumidity(void) {
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
   
   int count = 0;
-  while (count < 15) {
+  while (count < 15) {                          // loop for 3 seconds (15x200ms)
     count++;
     
-    display.clearDisplay();
+    display.clearDisplay();                     // clear the display buffer
     
     display.setCursor(0,4);
     display.print("Humidity:");
   
     display.setCursor(40,44);
-    display.print(si7021.readHumidity());
+    display.print(si7021.readHumidity());       // print to display buffer the humidity value from Si7021 sensor
     display.print(" %");
-    display.display();
+    display.display();                          // update display to show what is on the buffer
     
-    delay(200);
+    delay(200);                                 // wait 200 ms
   }
 }
 
+/* Show current air pressure on screen */
 void showpressure(void) {
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
   
   int count = 0;
-  while (count < 15) {
+  while (count < 15) {                          // loop for 3 seconds (15x200ms)
     count++;
     
-    display.clearDisplay();
+    display.clearDisplay();                     // clear the display buffer
     
     display.setCursor(0,4);
     display.print("Pressure:");
   
     display.setCursor(0,34);
-    display.print(bmp280.readPressure());
+    display.print(bmp280.readPressure());       // print to display buffer the pressure value from BMP280 sensor
 
     display.setCursor(80,50);
     display.print(" Pa");
-    display.display();
+    display.display();                          // update display to show what is on the buffer
     
-    delay(200);
+    delay(200);                                 // wait 200 ms
   }
 }
 
+/* Show current adjusted altitude on screen */
 void showaltitude(void) {
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
   
   int count = 0;
-  while (count < 15) {
+  while (count < 15) {                                // loop for 3 seconds (15x200ms)
     count++;
     
-    display.clearDisplay();
+    display.clearDisplay();                           // clear the display buffer
     
     display.setCursor(0,4);
     display.print("Altitude:");
   
     display.setCursor(30,44);
-    display.print(bmp280.readAltitude(1013.25));
+    display.print(bmp280.readAltitude(seaLevelhPa));  // print to display buffer the altitude value from BMP280 sensor
     display.print(" m");
-    display.display();
+    display.display();                                // update display to show what is on the buffer
     
-    delay(200);
+    delay(200);                                       // wait 200 ms
   }
 }
 
+/* Show current acceleration on the X axis */
 void showxaccel(void) {
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
 
-  sensors_event_t accel;
-  sensors_event_t gyro;
-  sensors_event_t temp;
+  sensors_event_t accel;                              // create sensor event structure for acceleration
+  sensors_event_t gyro;                               // create sensor event structure for gyroscope
+  sensors_event_t temp;                               // create sensor event structure for temperature
   
   int count = 0;
-  while (count < 60) {
+  while (count < 60) {                                // loop for 3 seconds (60x50ms)
     count++;
 
-    lsm6ds3.getEvent(&accel, &gyro, &temp);
+    lsm6ds3.getEvent(&accel, &gyro, &temp);           // get a new normalized sensor event
     
-    display.clearDisplay();
+    display.clearDisplay();                           // clear the display buffer
     
     display.setCursor(0,4);
     display.print("X accel:");
   
     display.setCursor(40,34);
-    display.print(accel.acceleration.x);
+    display.print(accel.acceleration.x);              // print to display buffer the X acceleration value from LSM6DS3 sensor
       
     display.setCursor(50,50);
     display.print(" m/s^2");
-    display.display();
+    display.display();                                // update display to show what is on the buffer
     
-    delay(50);
+    delay(50);                                        // wait 50 ms
   }
 }
 
+/* Show current acceleration on the Y axis */
 void showyaccel(void) {
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
@@ -421,6 +440,7 @@ void showyaccel(void) {
     display.setCursor(50,50);
     display.print(" m/s^2");
     display.display();
+    
     delay(50);
   }
 }

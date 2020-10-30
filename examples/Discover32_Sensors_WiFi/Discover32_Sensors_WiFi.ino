7e2857773c6055d30278d24338021391089cb549
FiCacador <ficacador@gmail.com>
2020-10-30 11:24:15 +0000

    Improve Sensors WiFi example

--- examples/Discover32_Sensors_WiFi/Discover32_Sensors_WiFi.ino
+++ examples/Discover32_Sensors_WiFi/Discover32_Sensors_WiFi.ino
@@ -4,27 +4,33 @@
 #include <Adafruit_LSM6DS3.h>
 #include <PCF85063TP.h>
 #include <Adafruit_Si7021.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <WiFi.h>
 #include <WebServer.h>
 #include <time.h>
   
-#define I2C_SDA 21
-#define I2C_SCL 22
-#define BMP_I2C_ADDR 0x76
-#define LSM_I2C_ADDR 0x6B
+#define I2C_SDA 21                      // I2C data line
+#define I2C_SCL 22                      // I2C clock line
+#define BMP_I2C_ADDR 0x76               // BMP280 I2C address
+#define LSM_I2C_ADDR 0x6B               // LSM6DS3 I2C address
+#define SSD_I2C_ADDR 0x3C               // SSD1306 I2C address
+#define SCREEN_WIDTH 128                // OLED screen width
+#define SCREEN_HEIGHT 64                // OLED screen height
 
 Adafruit_BMP280 bmp280;
 Adafruit_LSM6DS3 lsm6ds3;
 PCD85063TP RTclock;
 Adafruit_Si7021 si7021;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 WebServer server(80);
 
-const char* ssid     = "yourssid";
-const char* password = "yourpassword";
+const char* ssid     = "yourssid";        // replace with the name of the WiFi netwoork
+const char* password = "yourpassword";    // replace with the WiFi network password
+const char* ntpServer = "pool.ntp.org";   // ntp pool server
+const long gmtOffset = 0;                 // Greenwich Mean Time (GMT) offset, in hours
+float seaLevelhPa = 1025.0;               // local sea level pressure, in hPa, to adjust altitude
 bool led_enabled = false;
-const char* ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = 0;
-const int daylightOffset_sec = 3600;
 
 void setup() {
   Wire.begin(I2C_SDA, I2C_SCL);
@@ -44,28 +50,46 @@ void setup() {
     Serial.println("Could not find a valid Si7021 sensor!");
     while (1);
   }
-
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SSD_I2C_ADDR)) {
+    Serial.println("Could not find a valid SSD1306 screen!");
+    while (1);
+  }
   RTclock.begin();
 
   pinMode(17, OUTPUT);
+  digitalWrite(17, !LOW);                                       // turn off led (logic inverted)
 
+  display.clearDisplay();                                       // clear the display buffer
+  display.setTextColor(SSD1306_WHITE);                          // set draw color to white
 }
 
 void loop() {
   if (WiFi.status() != WL_CONNECTED) {
     Serial.print("Connecting to ");
     Serial.println(ssid);
+
+    display.clearDisplay();                                     // clear the display buffer
+    display.setTextSize(1);                                     // set text size to small (1:1 scale)
+    display.setCursor(0,20);                                    // start at position (X,Y), starting from top left
+    display.println("Connecting to");                           // print line to display buffer
+    display.println(ssid);
+    display.display();                                          // update display to show what is on the buffer
+    
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
       led_enabled = !led_enabled;
       digitalWrite(17, led_enabled);
       delay(500);
     }
-    Serial.println("WiFi connected.");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
 
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    digitalWrite(17, !LOW);                                     // turn off led (logic inverted)
+    
+    Serial.println("WiFi connected");
+
+    display.println("WiFi connected");
+    display.display();
+    
+    configTime(gmtOffset * 3600, 0, ntpServer);
     updateRTclock();
 
     server.on("/", handleRoot);
@@ -74,8 +98,17 @@ void loop() {
       server.send(404, "text/plain", "Page not found");
     });
     server.begin();
+    
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 
-    digitalWrite(17, LOW);
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setCursor(0,20);
+    display.println(WiFi.localIP());
+    display.display();
+    
+    digitalWrite(17, !HIGH);                                    // turn on led (logic inverted)
   }
   server.handleClient();
 }
@@ -85,6 +118,11 @@ void updateRTclock() {
   if(!getLocalTime(&timeinfo)){
     Serial.println("Failed to obtain time");
     return;
+  }
+
+  if (timeinfo.tm_isdst > 0) {
+    configTime(gmtOffset * 3600, 3600, ntpServer);
+    getLocalTime(&timeinfo);
   }
   
   RTclock.stopClock();
@@ -127,10 +165,10 @@ void handleRoot() {
 "<html>\
   <head>\
     <meta http-equiv='refresh' content='5'/>\
-    <title>Discover Board</title>\
+    <title>Discover32</title>\
   </head>\
   <body>\
-    <h1>Discover Board Sensors</h1>\
+    <h1>Discover32 Sensors</h1>\
     <p>%s %d/%d/%d %02d:%02d:%02d</p>\
     <p>Temperature: %.2f C</p>\
     <p>Humidity: %.1f %%</p>\
@@ -141,7 +179,7 @@ void handleRoot() {
   </body>\
 </html>",
   convertDayOfWeek(RTclock.dayOfWeek), RTclock.year + 2000, RTclock.month, RTclock.dayOfMonth, RTclock.hour, RTclock.minute, RTclock.second,
-    si7021.readTemperature(), si7021.readHumidity(), bmp280.readPressure(), bmp280.readAltitude(1013.25),
+    si7021.readTemperature(), si7021.readHumidity(), bmp280.readPressure(), bmp280.readAltitude(seaLevelhPa),
     accel.acceleration.x, accel.acceleration.y);
 
   server.send(200, "text/html", message);
@@ -173,7 +211,7 @@ void handleAPI() {
 \"y-accel\": %.2f\
 }", 
   convertDayOfWeek(RTclock.dayOfWeek), RTclock.year + 2000, RTclock.month, RTclock.dayOfMonth, RTclock.hour, RTclock.minute, RTclock.second,
-    si7021.readTemperature(), si7021.readHumidity(), bmp280.readPressure(), bmp280.readAltitude(1013.25),
+    si7021.readTemperature(), si7021.readHumidity(), bmp280.readPressure(), bmp280.readAltitude(seaLevelhPa),
     accel.acceleration.x, accel.acceleration.y);
 
   server.send(200, "application/json", message);

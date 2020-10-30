e7ed2d2e67cd63396c6aa7b682e57a1ce50fa039
FiCacador <ficacador@gmail.com>
2020-10-30 17:14:25 +0000

    Add comments to Sensors WiFi

--- examples/Discover32_Sensors_WiFi/Discover32_Sensors_WiFi.ino
+++ examples/Discover32_Sensors_WiFi/Discover32_Sensors_WiFi.ino
@@ -32,72 +32,74 @@ const long gmtOffset = 0;                 // Greenwich Mean Time (GMT) offset, i
 float seaLevelhPa = 1025.0;               // local sea level pressure, in hPa, to adjust altitude
 bool led_enabled = false;
 
+/* Run setup once */
 void setup() {
   Wire.begin(I2C_SDA, I2C_SCL);
   Serial.begin(115200);
 
   Serial.println("Discover32 Sensors WiFi test");
 
-  if (!bmp280.begin(BMP_I2C_ADDR, BMP280_CHIPID)) {
+  if (!bmp280.begin(BMP_I2C_ADDR, BMP280_CHIPID)) {                 // try to initialize BMP280 sensor
     Serial.println("Could not find a valid BMP280 sensor!");
-    while (1);
+    while (1);                                                      // loop forever
   }
-  if (!lsm6ds3.begin_I2C(LSM_I2C_ADDR, &Wire, LSM6DS3_CHIP_ID)) {
+  if (!lsm6ds3.begin_I2C(LSM_I2C_ADDR, &Wire, LSM6DS3_CHIP_ID)) {   // try to initialize LSM6DS3 sensor
     Serial.println("Could not find a valid LSM6DS3 sensor!");
-    while (1);
+    while (1);                                                      // loop forever
   }
-  if (!si7021.begin()) {
+  if (!si7021.begin()) {                                            // try to initialize Si7021 sensor
     Serial.println("Could not find a valid Si7021 sensor!");
-    while (1);
+    while (1);                                                      // loop forever
   }
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SSD_I2C_ADDR)) {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SSD_I2C_ADDR)) {          // try to initialize SSD1306 display
     Serial.println("Could not find a valid SSD1306 screen!");
-    while (1);
+    while (1);                                                      // loop forever
   }
-  RTclock.begin();
+  RTclock.begin();                                                  // Initialize Real-Time clock
 
-  pinMode(17, OUTPUT);
-  digitalWrite(17, !LOW);                                       // turn off led (logic inverted)
+  pinMode(17, OUTPUT);                                              // set led pin 17 as output
+  digitalWrite(17, !LOW);                                           // turn off led (logic inverted)
 
-  display.clearDisplay();                                       // clear the display buffer
-  display.setTextColor(SSD1306_WHITE);                          // set draw color to white
+  display.clearDisplay();                                           // clear the display buffer
+  display.setTextColor(SSD1306_WHITE);                              // set draw color to white
 }
 
+/* Loop to keep verifying WiFi connection and handling server requests */
 void loop() {
-  if (WiFi.status() != WL_CONNECTED) {
+  if (WiFi.status() != WL_CONNECTED) {                              // verify there is no connection
     Serial.print("Connecting to ");
     Serial.println(ssid);
 
-    display.clearDisplay();                                     // clear the display buffer
-    display.setTextSize(1);                                     // set text size to small (1:1 scale)
-    display.setCursor(0,20);                                    // start at position (X,Y), starting from top left
-    display.println("Connecting to");                           // print line to display buffer
+    display.clearDisplay();                                         // clear the display buffer
+    display.setTextSize(1);                                         // set text size to small (1:1 scale)
+    display.setCursor(0,20);                                        // start at position (X,Y), starting from top left
+    display.println("Connecting to");                               // print line to display buffer
     display.println(ssid);
-    display.display();                                          // update display to show what is on the buffer
+    display.display();                                              // update display to show what is on the buffer
     
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-      led_enabled = !led_enabled;
-      digitalWrite(17, led_enabled);
-      delay(500);
+    WiFi.begin(ssid, password);                                     // connect to the specified WiFi network
+    while (WiFi.status() != WL_CONNECTED) {                         // verify it hasn't connected yet
+      led_enabled = !led_enabled;                                   // toggle led state
+      digitalWrite(17, led_enabled);                                // turn led on or off
+      delay(500);                                                   // wait 500 ms
     }
 
-    digitalWrite(17, !LOW);                                     // turn off led (logic inverted)
+    digitalWrite(17, !LOW);                                         // turn off led (logic inverted)
     
     Serial.println("WiFi connected");
 
     display.println("WiFi connected");
     display.display();
     
-    configTime(gmtOffset * 3600, 0, ntpServer);
-    updateRTclock();
+    configTime(gmtOffset * 3600, 0, ntpServer);                     // configure time
+    updateRTclock();                                                // call function to update the real-time clock
 
-    server.on("/", handleRoot);
-    server.on("/api", handleAPI);
-    server.onNotFound([]() {
-      server.send(404, "text/plain", "Page not found");
+    server.on("/", handleRoot);                                     // function handleRoot runs when a request to root is received
+    server.on("/api", handleAPI);                                   // function handleAPI runs when a request to /api is received
+    server.onNotFound([]() {                                        // anonymous function with a simple server response
+      server.send(404, "text/plain", "Page not found");             //  runs when a request is not found
     });
-    server.begin();
+    server.begin();                                                 // start server
     
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
@@ -108,31 +110,34 @@ void loop() {
     display.println(WiFi.localIP());
     display.display();
     
-    digitalWrite(17, !HIGH);                                    // turn on led (logic inverted)
+    digitalWrite(17, !HIGH);                                        // turn on led (logic inverted)
   }
-  server.handleClient();
+  server.handleClient();                                            // handle server requests
 }
 
+/* Function to update the real-time clock from ntp */
 void updateRTclock() {
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
+  struct tm timeinfo;                                               // create time structure
+  
+  if(!getLocalTime(&timeinfo)){                                     // try to get time info and save it to the timeinfo structure
     Serial.println("Failed to obtain time");
     return;
   }
 
-  if (timeinfo.tm_isdst > 0) {
-    configTime(gmtOffset * 3600, 3600, ntpServer);
-    getLocalTime(&timeinfo);
+  if (timeinfo.tm_isdst > 0) {                                      // verify if it's summer time (DST)
+    configTime(gmtOffset * 3600, 3600, ntpServer);                  // configure time with summer time offset
+    getLocalTime(&timeinfo);                                        // get time info and save it to the timeinfo structure
   }
   
-  RTclock.stopClock();
-  RTclock.fillDayOfWeek(timeinfo.tm_wday);
-  RTclock.fillByYMD(timeinfo.tm_year + 1900, timeinfo.tm_mon + 1, timeinfo.tm_mday);
-  RTclock.fillByHMS(timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
-  RTclock.setTime();
-  RTclock.startClock();
+  RTclock.stopClock();                                                                // stop the real-time clock
+  RTclock.fillDayOfWeek(timeinfo.tm_wday);                                            // fill week day from timeinfo structure
+  RTclock.fillByYMD(timeinfo.tm_year + 1900, timeinfo.tm_mon + 1, timeinfo.tm_mday);  // fill year, month and day from timeinfo structure
+  RTclock.fillByHMS(timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);              // fill hour, minute and second from timeinfo structure
+  RTclock.setTime();                                                                  // set the filled time
+  RTclock.startClock();                                                               // start the real-time clock
 }
 
+/* Function that receives week day info and returns the corresponding string */
 char* convertDayOfWeek(int nDayOfWeek) {
   switch (nDayOfWeek) {
     case 0:
@@ -152,6 +157,7 @@ char* convertDayOfWeek(int nDayOfWeek) {
   }
 }
 
+/* Server response to root, a simple web page is delivered */
 void handleRoot() {
   RTclock.getTime();
   sensors_event_t accel;
@@ -185,6 +191,7 @@ void handleRoot() {
   server.send(200, "text/html", message);
 }
 
+/* Server response to /api, the values are delivered in JSON format */
 void handleAPI() {
   RTclock.getTime();
   sensors_event_t accel;
